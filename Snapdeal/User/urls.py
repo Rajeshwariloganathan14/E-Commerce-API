25e4e924ba4efa097a55d8d2f73c26ba7a147a66
Rajeshwariloganathan14 <raji1442006@gmail.com>
2025-08-26 14:21:09 +0530

    email in form

--- Snapdeal/User/urls.py
+++ Snapdeal/User/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns=[
     path("email/",views.EmailView.as_view(),name="Email"),
-    path("otp/<str:email>/",views.OTPView.as_view(),name="otp"),
-    path("setPassword/<str:email>/",views.SetPasswordView.as_view(),name="Sign Up")
+    path("otp/",views.OTPView.as_view(),name="otp"),
+    path("setPassword/",views.SetPasswordView.as_view(),name="Sign Up")
     
 ]
 
